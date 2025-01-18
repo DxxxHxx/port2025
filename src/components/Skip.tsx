3ce9a75be686abc0f268c936ea8dfeb76268e66f
DxxxHxx <donghun1163@gmail.com>
2025-01-18 21:46:11 +0900

    design: 헤더 완료

--- src/components/Skip.tsx
+++ src/components/Skip.tsx
@@ -1,3 +1,13 @@
 export default function Skip() {
-  return <div>skip</div>;
+  return (
+    <div id="skip">
+      <a href="#header">header</a>
+      <a href="#intro">intro</a>
+      <a href="#skill">skill</a>
+      <a href="#site">site</a>
+      <a href="#port">port</a>
+      <a href="#contact">contact</a>
+      <a href="#footer">footer</a>
+    </div>
+  );
 }
