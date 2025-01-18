742b77206c87b4013122eff35771512890c68655
DxxxHxx <donghun1163@gmail.com>
2025-01-18 23:00:41 +0900

    design: 스킬 완료

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,3 +1,22 @@
+import { skillText } from "../constants";
+
 export default function Skill() {
-  return <div>Skill</div>;
+  return (
+    <section id="skill">
+      <div className="skill__inner">
+        <h2 className="skill__title">
+          Challenge <em>나의 도전</em>
+        </h2>
+        <div className="skill__desc">
+          {skillText.map((item, index) => (
+            <div>
+              <span>{index + 1}.</span>
+              <h3>{item.title}</h3>
+              <p>{item.desc}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
