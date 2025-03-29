abf6c28077e50ed31facaeea8663c40c75d53ea4
DxxxHxx <donghun1163@gmail.com>
2025-03-29 22:24:46 +0900

    design: skill 영역 애니메이션 추가

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,4 +1,5 @@
 import { skillText } from "../constants/skillConstant";
+import { motion } from "framer-motion";
 
 export default function Skill() {
   return (
@@ -9,11 +10,27 @@ export default function Skill() {
         </h2>
         <div className="skill__desc">
           {skillText.map((item, index) => (
-            <div key={index}>
+            <motion.div
+              variants={{
+                init: {
+                  x: 100,
+                  opacity: 0,
+                },
+                inView: {
+                  x: 0,
+                  opacity: 1,
+                  transition: { duration: 0.5 },
+                },
+              }}
+              initial="init"
+              whileInView="inView"
+              viewport={{ margin: "200px 0px 0px 0px", amount: 0.8 }}
+              key={index}
+            >
               <span>{index + 1}.</span>
               <h3>{item.title}</h3>
               <p>{item.desc}</p>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
